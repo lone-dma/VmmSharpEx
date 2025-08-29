1ccdf993d4b9bdccb955e2d6616bfd003f4ba33c
boggle4539 <225045747+boggle4539@users.noreply.github.com>
2025-08-29 10:41:14 -0500

    Improve tests/benchmarking

--- src/VmmSharpEx_Tests/Program.cs
+++ src/VmmSharpEx_Tests/Program.cs
@@ -1,23 +1,70 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
 using System.Text;
 using VmmSharpEx;
-using VmmSharpEx.Scatter;
 
 namespace VmmSharpEx_Tests;
 
 internal unsafe class Program
 {
-    private static void Main()
+    /// <summary>
+    /// VMM Instance for this App Domain.
+    /// </summary>
+    internal static Vmm Vmm { get; }
+    static Program()
     {
+        // Enable Unicode output in console
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.InputEncoding = Encoding.Unicode;
+        // Optimize this process for performance
+        Thread.CurrentThread.Priority = ThreadPriority.Highest;
+        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+        // Initialize VMM
         string[] args = new[]
-        {
+{
             "-printf",
             "-v",
             "-device",
             "fpga",
             "-waitinitialize"
         };
-        using var vmm = new Vmm(args);
-        ScatterMap_Example.Run(vmm);
+        Vmm = new Vmm(args);
+        AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+    }
+
+    private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
+    {
+        Vmm.Dispose();
+    }
+
+    private static void Main()
+    {
+        ScatterMap_Example.Run(Vmm);
+        RunBenchmark(
+            method: ScatterBenchmark.Run, 
+            name: "Scatter Benchmark", 
+            count: 10);
+    }
+
+    private static void RunBenchmark(Action method, string name, int count)
+    {
+        var list = new List<TimeSpan>(count);
+        for (int i = 0; i <= count; i++)
+        {
+            var sw = Stopwatch.StartNew();
+            method();
+            var elapsed = sw.Elapsed;
+            if (i > 0) // Already Jitted
+            {
+                list.Add(elapsed);
+                Console.WriteLine($"{name} #{i} Runtime:\n" +
+                    $"• {elapsed.TotalMilliseconds} ms\n" +
+                    $"• {elapsed.TotalMicroseconds} µs\n" +
+                    $"• {elapsed.Ticks} ticks");
+            }
+        }
+        Console.WriteLine($"=== {name} Completed ===\n" +
+            $"• {list.Select(x => x.TotalMilliseconds).Average()} ms avg\n" +
+            $"• {list.Select(x => x.TotalMicroseconds).Average()} µs avg\n" +
+            $"• {list.Select(x => x.Ticks).Average()} ticks avg");
     }
 }
---
+++ src/VmmSharpEx_Tests/ScatterBenchmark.cs
@@ -0,0 +1,39 @@
+﻿using VmmSharpEx;
+using VmmSharpEx.Scatter;
+
+namespace VmmSharpEx_Tests
+{
+    internal static class ScatterBenchmark
+    {
+        private static readonly Vmm _vmm;
+        private static readonly uint _pid;
+        private static readonly ulong _baseAddress;
+        static ScatterBenchmark()
+        {
+            _vmm = Program.Vmm;
+            if (!_vmm.PidGetFromName("explorer.exe", out _pid))
+                throw new InvalidOperationException("Failed to get PID");
+            _baseAddress = _vmm.ProcessGetModuleBase(_pid, "explorer.exe");
+            if (_baseAddress == 0)
+                throw new InvalidOperationException("Failed to get base address");
+        }
+
+        public static void Run()
+        {
+            using var map = new ScatterReadMap(_vmm, _pid);
+            var rd1 = map.AddRound(useCache: true);
+            for (int i = 0; i < 100; i++)
+            {
+                rd1[i].AddArrayEntry<byte>(0, _baseAddress + 0x1000ul * (uint)i, 0x1000);
+                rd1[i].Completed += (sender, cb) =>
+                {
+                    if (cb.TryGetArray<byte>(0, out var bytes))
+                    {
+                        
+                    }
+                };
+            }
+            map.Execute();
+        }
+    }
+}
